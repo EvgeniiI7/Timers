70b63acbe6158bedd1def3f1f7ccf237090a11fe
Evgenii Ivanov <evgenii7.ivanov@yandex.ru>
2022-05-13 08:50:13 -0400

    final commit

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var startBtn = document.getElementById('start');
+
+var message =
+  'Congratulations! Now you are ready to build your own timer! Good luck!';
+var words = message.split(' '); // --> inside in split parentheses are( space (or what ever you want .!*_-)) , so its split the words  [Congratulations!, Now, you, ... ]
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 5;
+
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds left'
+      timeLeft --;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second left'
+      timeLeft --;
+    } else {
+      timerEl.textContent = "";
+      clearInterval (timeInterval);
+      displayMessage();
+    }
+  },1000);
+}
+
+// Displays the message by one word at a time.
+function displayMessage() {
+  var wordCount = 0;
+
+  // Uses the `setInterval()` method to call a function to be executed every 300 milliseconds or what ever you want 
+  var msgInterval = setInterval(function() {
+    if (words[wordCount] === undefined) {
+      //words mean that is array.   wordCount = 0index= Congratulations!  increment words by one 300mlsec,
+      clearInterval(msgInterval);
+    } else {
+      mainEl.textContent = words[wordCount];
+      wordCount++;
+    }
+  }, 300);
+}
+// start countdown only when you click on the button
+startBtn.onclick = countdown;
+
+
+function circularText(txt, radius, classIndex) {
+  txt = txt.split(""),
+    classIndex = document.getElementsByClassName('start')[classIndex];
+
+  var deg = 360 / txt.length,
+    origin = 0;
+
+  txt.forEach((ea) => {
+    ea = `<p style='margin:center;display:inline-block;height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+    classIndex.innerHTML += ea;
+    origin += deg;
+  });
+}
+
+circularText("Please, press the button!", 100, 0);
